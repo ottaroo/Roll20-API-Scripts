671290e9698211d4036445edaa8ef9a4c74271eb
ottaroo <59054246+ottaroo@users.noreply.github.com>
2026-03-08 00:28:33 +0100

    wildshape v1

--- README.md
+++ README.md
@@ -1,2 +1,107 @@
-# Roll20-API-Scripts
-Just a simple script to handle my wildshape + some other stuff. Will probably need some rework once Beacon becomes available.
+﻿# D&D Roll20 API Script: dndscripts.js
+This script was designed to be used with the Roll20 API.  
+It's not optimal but tries to work without the new Beacon API, since its not yet available.  
+
+Primary function is to swap token when doing wildshape as a druid.
+
+## Commands
+
+``` 
+!wildshape
+!wildshap-cancel
+!fixtoken
+!adjustsize <size>
+!resetsize
+```
+
+### !wildshape
+To use this command you need to have a roll20 D&D 5e 2024 character sheet.  
+For each shape you know, you add another character sheet, but you choose the NPC edition.  
+In addition you must give the NPC sheet name to match your main PC character sheet + ' - Beast Name'.  
+For example:  
+You have a PC character sheet named `Diarmuid O’Duibne`  
+And you know the wildshape forms of Brown bear and Tiger you will add two NPC sheets:  
+One named `Diarmuid O’Duibne - Brown bear`    
+And `Diarmuid O’Duibne - Tiger`  
+And so on for each shape you know.  
+
+Select your token and type `!wildshape`  
+You will be prompted in the chat with a button for each available form.  
+Clicking the button will swap your token with the corresponding form.  
+The prompt also contains a button to cancel the wildshape.
+
+### !wildshape-cancel
+To cancel the wildshape, select your token and type `!wildshape-cancel`.  
+Or you can click the button in the wildshape prompt.
+
+### !adjustsize
+This command allows you to adjust the size of the token.    
+The size of the token is determined by the `size` attribute.    
+The `size` attribute can be set to one of the following values:  
+* `t` or `T` - which is the tiny size of a token.    
+* `s` or `S` - which is the small size of a token.    
+* `m` or `M` - which is the medium size of a token (default).      
+* `l` or `L` - which is the large size of a token.   
+* `h` or `H` - which is the huge size of a token.   
+* `g` or `G` - which is the gargantuan size of a token.    
+* `c` or `C` - which is the colossal size of a token.
+
+Example usage:
+```
+!adjustsize h
+```
+This will set the size of the current selected token to huge.  
+
+### !resetsize
+This command resets the size of the selected token to the default value.
+
+### !fixtoken
+This is a command to set most of the token's attributes to the default values we use in our campaign.  
+It sets the following attributes:  
+
+* `showname: true`  
+   this will display name plate to controllers of the token.      
+* `showplayers_name: true`  
+   this will display token name plate to other players.    
+* `showplayers_bar1: true`  
+   this will display HP bar to other players.    
+* `playersedit_bar1: true`  
+   this will allow players to edit HP bar.                
+* `bar1_num_permission: "everyone"`  
+   this will allow anyone to see the numeric value of the HP bar.    
+* `bar1_link: "hp"`  
+   this will link the HP bar to the linked character sheet's HP attribute.     
+* `showplayers_bar2: true`  
+  this will display Temporary HP bar to other players.
+* `playersedit_bar2: true`  
+  this will allow players to edit Temporary HP bar.
+* `bar2_num_permission: "everyone"`  
+  this will allow anyone to see the numeric value of the Temporary HP bar.
+* `bar2_link: "hp_temp"`  
+  this will link the Temporary HP bar to the linked character sheet's Temporary HP attribute.  
+* `showplayers_bar3: true`  
+  this will display custom bar to other players.
+* `playersedit_bar3: true`  
+  this will allow players to edit their custom bar.  
+* `bar3_num_permission: "everyone"`  
+  this will allow anyone to see the numeric value of the custom bar.  
+* `showplayers_aura1: true`  
+  this will allow players to see the first aura if it exists on the token.  
+* `showplayers_aura2: true`  
+  this will allow players to see the second aura if it exists on the token.  
+* `playersedit_aura1: true`  
+  this will allow players to edit their first aura on the token.  
+* `playersedit_aura2: true`  
+  this will allow players to edit their second aura on the token.  
+
+The GM must complete the setup of the token:  
+The GM will need to update `Token Vision` under `Dyanmic Lighting` manually.  
+It did not seem to work correctly at the moment by just updating these values from the script.  
+When all values are set, the GM can click `Update Default Token` under `Details` to save all the changes for future use of the token.  
+Now every time the GM or the players add the token to the map, the token will be ready to use.  
+
+**NOTE:**  
+The GM can select multiple tokens and run the command on them in a single operation.  
+However, the script does not check if the token is controlled by a player if executed by a GM.  
+Which means the GM could run this on NPC token and set everything visible accidentally.  
+
---
+++ dndscripts.js
@@ -0,0 +1,437 @@
+﻿// ===== Main command: show menu or shift =====
+on("chat:message", async function(msg) {
+    if (msg.type !== "api") return;
+
+    let args = msg.content.split(" ");
+
+    if (args.length === 0) return;
+
+    let command = args[0].toLowerCase();
+
+    switch(command) {
+
+        case "!wildshape-cancel":
+            await cancelWildShape(msg);
+            break;
+        case "!wildshape-shift":
+            await shiftWildShape(msg, args);
+            break;
+        case "!wildshape":
+            await createWildShapeMenu(msg);
+            break;
+        case "!fixtoken":
+            await fixToken(msg);
+            break;
+        case "!adjustsize":
+            await adjustSize(msg, args);
+            break;
+        case "!resetsize":
+            await resetSize(msg);
+            break;
+        default:
+            break;
+
+    }
+});
+
+
+// ===== Helper: get default token for character ====
+function getDefaultTokenAsync(charObj) {
+    return new Promise(resolve => {
+        charObj.get("defaulttoken", json => resolve(json));
+    });
+}
+
+// ===== Helper: clone token properties =====
+function cloneTokenProps(token) {
+    const keys = [
+        "pageid","layer","left","top","width","height","rotation",
+        "flipv","fliph","name","showname","bar1_value","bar1_max",
+        "bar2_value","bar2_max","bar3_value","bar3_max",
+        "bar1_link","bar2_link","bar3_link",
+        "aura1_radius","aura1_color","aura1_square",
+        "aura2_radius","aura2_color","aura2_square",
+        "tint_color","statusmarkers","showplayers_name",
+        "showplayers_bar1","showplayers_bar2","showplayers_bar3",
+        "showplayers_aura1","showplayers_aura2",
+        "light_radius","light_dimradius","light_angle",
+        "light_otherplayers","light_hassight","light_losangle",
+        "lastmove","controlledby"
+    ];
+    let props = {};
+    keys.forEach(k => props[k] = token.get(k));
+    return props;
+}
+
+
+async function getToken(msg, selectedId = null) {
+    if (!msg.selected || msg.selected.length === 0) {
+        sendChat("WildShape", "/w " + msg.who + " No token selected.");
+        return null;
+    }
+    let token = getObj("graphic", selectedId ?? msg.selected[0]._id);
+    if (!token) return null;
+
+    let pcChar = getObj("character", token.get("represents"));
+    if (!pcChar) {
+        sendChat("WildShape", "/w " + msg.who + " Selected token is not linked to a character.");
+        return false;
+    }
+
+    let isGm = playerIsGM(msg.playerid);
+    let tokenIsControlledBy = token.get("controlledby");
+    let allowedControl = isGm;
+    if (!allowedControl) {
+        for (let controlledBy of tokenIsControlledBy) {
+            if (controlledBy === msg.playerId) {
+                allowedControl = true;
+                break;
+            }
+        }
+    }
+    if (!allowedControl) {
+        sendChat("WildShape", "/w " + msg.who + " You do not have permission to use this token.");
+        return null;
+    }
+    return token;
+}
+
+
+
+
+async function createWildShapeMenu(msg) {
+    const token = await getToken(msg);
+    if (!token) return;
+
+    let pcChar = getObj("character", token.get("represents"));
+    let pcName = pcChar.get("name");
+    let prefix = pcName + " - ";
+
+    // Find all NPC sheets matching "<PC Name> - <Form>"
+    let npcs = findObjs({ type: "character" })
+        .filter(c => c.get("name").startsWith(prefix));
+
+    if (npcs.length === 0) {
+        sendChat("WildShape",
+            "/w " + msg.who +
+            " No Wild Shape forms found. Expected names like:<br>" +
+            `"${pcName} - Bear" or "${pcName} - Tiger"`
+        );
+        return;
+    }
+
+    // Build menu
+    let buttons = npcs.map(npc => {
+        let formName = npc.get("name").substring(prefix.length);
+        return "[" + formName + "](!wildshape-shift " + pcChar.id + " " + npc.id + " " + token.id + ")";
+    }).concat([
+        "[Cancel Wildshape](!wildshape-cancel)"
+    ]);
+
+    sendChat("WildShape",
+        "/w " + msg.who +
+        " Choose your Wild Shape:<br>" +
+        buttons.join(" ")
+    );
+}
+
+
+async function cancelWildShape(msg)
+{
+    const wsToken = await getToken(msg);
+    if (!wsToken) return;
+
+    let pc = findObjs({ type: "character" })
+        .filter(c => c.get("name") === wsToken.get("name"))[0];
+
+    const defaultTokenJSON = await getDefaultTokenAsync(pc);
+
+    if (!defaultTokenJSON) {
+        sendChat("WildShape", `/w ${msg.who} ERROR: PC '${pc.get("name")}' has no default token.`);
+        return;
+    }
+
+    let dt;
+    try {
+        dt = JSON.parse(defaultTokenJSON);
+    } catch {
+        sendChat("WildShape", `/w ${msg.who} ERROR: Default token for '${pc.get("name")}' is corrupted.`);
+        return;
+    }
+
+    if (!dt.imgsrc || dt.imgsrc.includes("thumb")) {
+        sendChat("WildShape", `/w ${msg.who} ERROR: Default token for '${pc.get("name")}' has an invalid image.`);
+        return;
+    }
+
+    // --- Clone token props ---
+    let props = cloneTokenProps(wsToken);
+    props.represents = pc.id;
+    props.imgsrc = dt.imgsrc;
+    props.width = dt.width || wsToken.get("width");
+    props.height = dt.height || wsToken.get("height");
+
+    // --- Create new token ---
+    createObj("graphic", props);
+    sendChat("WildShape", `/em transforms back into ${pc.get("name")}!`);
+    wsToken.remove();
+}
+
+async function shiftWildShape(msg, args) {
+    if (args.length < 4) return;
+
+    let pcId = args[1];
+    let npcId = args[2];
+    let tokenId = args[3];
+
+    let pcChar = getObj("character", pcId);
+    let npcChar = getObj("character", npcId);
+    let token = await getToken(msg);
+
+    if (!pcChar || !npcChar || !token) return;
+    if (token.id !== tokenId) return;
+
+    if (!pcChar || pcChar.get("type") !== "character") { log("ERROR: Token is not linked to a character sheet"); return; }
+
+
+    let pcName = pcChar.get("name");
+    let pcSheet = findObjs({
+        _type: "character",
+        name: pcName
+    })[0];
+
+    await performWildShapeShift(pcChar, npcChar, token, msg.who);
+}
+
+
+
+async function performWildShapeShift(pcChar, npcChar, oldToken, who) {
+
+    const pcId = pcChar.id;
+    const npcId = npcChar.id;
+
+
+    // --- Get default token ---
+    const defaultTokenJSON = await getDefaultTokenAsync(npcChar);
+
+    if (!defaultTokenJSON) {
+        sendChat("WildShape", `/w ${who} ERROR: NPC '${npcChar.get("name")}' has no default token.`);
+        return;
+    }
+
+    let dt;
+    try {
+        dt = JSON.parse(defaultTokenJSON);
+    } catch {
+        sendChat("WildShape", `/w ${who} ERROR: Default token for '${npcChar.get("name")}' is corrupted.`);
+        return;
+    }
+
+    if (!dt.imgsrc || dt.imgsrc.includes("thumb")) {
+        sendChat("WildShape", `/w ${who} ERROR: Default token for '${npcChar.get("name")}' has an invalid image.`);
+        return;
+    }
+
+    // --- Clone token props ---
+    let props = cloneTokenProps(oldToken);
+    props.represents = pcId;
+    props.imgsrc = dt.imgsrc;
+    props.width = dt.width || oldToken.get("width");
+    props.height = dt.height || oldToken.get("height");
+
+    // --- Create new token ---
+    createObj("graphic", props);
+
+    sendChat("WildShape", `/em transforms into ${npcChar.get("name")}!`);
+
+    oldToken.remove();
+}
+
+async function fixToken(msg) {
+
+    if (!msg.selected || msg.selected.length === 0) {
+        sendChat("TokenSetup", "/w " + msg.who + " No tokens selected.");
+        return;
+    }
+
+    for(const sel of msg.selected){
+
+        let token = await getToken(msg, sel._id);
+        if (!token) continue;
+        let represents = token.get("represents");
+        let char = represents ? getObj("character", represents) : null;
+
+        let characterSheet = getCharacterSheetAsJson(char.id);
+        let darkvision = getDarkVision(characterSheet);
+
+        // --- APPLY TOKEN SETTINGS ---
+        token.set({
+            showname: true,          // Nameplate ON
+            showplayers_name: true,
+
+            // Bars visible & editable
+            showplayers_bar1: true,
+            showplayers_bar2: true,
+            showplayers_bar3: true,
+            playersedit_bar1: true,
+            playersedit_bar2: true,
+            playersedit_bar3: true,
+            bar1_link: "hp",
+            bar2_link: "hp_temp",
+            bar1_num_permission: "everyone",
+            bar2_num_permission: "everyone",
+            bar3_num_permission: "everyone",
+
+            // Auras visible & editable
+            showplayers_aura1: true,
+            showplayers_aura2: true,
+            playersedit_aura1: true,
+            playersedit_aura2: true,
+
+//            light_hassight: true,
+//            has_night_vision: darkvision > 0,
+//            night_vision_distance: darkvision
+
+        });
+
+    }
+
+    sendChat("TokenSetup", "/w " + msg.who + " Token properties updated.");
+}
+
+function getCharacterSheetAsJson(characterId) {
+    const store = findObjs({ type: 'attribute', characterid: characterId, name: 'store'})[0];
+
+    if (!store) {
+        throw new Error(`Character ${characterId} has no 'store' attribute.`);
+    }
+    return store.get("current");
+
+}
+
+function getDarkVision(characterJson) {
+    let total = 0;
+    let totalBase = 0;
+
+    function walk(node) {
+        if (node && typeof node === "object") {
+            const type = node.type;
+            const calc = node.calculation;
+
+            const matchesAbility =
+                typeof type === "string" &&
+                type.toLowerCase() === "sense";
+
+            const matchesCalc = calc === "Set Base" || calc === "Modify";
+
+            if (matchesAbility && matchesCalc && node.name === "Darkvision") {
+                const flatValue = node.valueFormula?.flatValue;
+                if (typeof flatValue === "number") {
+                    if (calc === "Set Base" && flatValue > totalBase) {
+                        totalBase = flatValue;
+                    }
+                    else {
+                        total += flatValue;
+                    }
+                }
+            }
+
+            for (const key in node) {
+                walk(node[key]);
+            }
+        }
+    }
+
+    walk(characterJson);
+    return total + totalBase;
+}
+
+async function adjustSize(msg, args) {
+
+    if (!msg.selected || msg.selected.length === 0) {
+        sendChat("TokenSetup", "/w " + msg.who + " No tokens selected.");
+        return;
+    }
+
+    if (args.length < 2) {
+        sendChat("TokenSetup", "/w " + msg.who + " Invalid arguments. Usage: !adjustsize <T,S,M,L,H,G>");
+        return;
+    }
+    
+    let width = 70;
+    let height = 70;
+    
+    switch (args[1].toLowerCase()) {
+        case "t":
+            width = 18;
+            height = 18;
+            break;
+        case "s":
+            width = 35;
+            height = 35;
+            break;
+        case "m":
+            width = 70;
+            height = 70;
+            break;
+        case "l":
+            width = 140;
+            height = 140;
+            break;
+        case "h":
+            width = 210;
+            height = 210;
+            break;
+        case "g":
+            width = 280;
+            height = 280;
+            break;
+        case "c":
+            width = 350;
+            height = 350;
+            break;
+        default:
+            break;
+    }
+    
+    
+    
+    for (const sel of msg.selected) {
+
+        let token = await getToken(msg, sel._id);
+        if (!token) continue;
+
+        
+        token.set({
+            width: width,
+            height: height
+        });
+        
+    }
+
+}
+
+async function resetSize(msg) {
+
+    const wsToken = await getToken(msg);
+    if (!wsToken) return;
+
+    let pc = findObjs({ type: "character" })
+        .filter(c => c.get("name") === wsToken.get("name"))[0];
+
+    const defaultTokenJSON = await getDefaultTokenAsync(pc);
+    let dt;
+    try {
+        dt = JSON.parse(defaultTokenJSON);
+    } catch {
+        sendChat("Adjustsize", `/w ${msg.who} ERROR: Default token for '${pc.get("name")}' is corrupted.`);
+        return;
+    }
+    
+    wsToken.set({
+        width: dt.width,
+        height: dt.height
+    });
+    
+}
+
